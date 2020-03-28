10a7b2507c9d340952b349668c236f6002f4ba13
Mitch Webster <mitchell.a.webster@gmail.com>
2020-03-28 16:56:29 -0700

    try to get 500

--- api/AddPhoto/index.js
+++ api/AddPhoto/index.js
@@ -2,11 +2,17 @@ var multipart = require("parse-multipart");
 
 module.exports = async function (context, req) {
     
-    var bodyBuffer = Buffer.from(request.body);
-    var boundary = multipart.getBoundary(request.headers['content-type']);
+    try {
+            var bodyBuffer = Buffer.from(request.body);
+            var boundary = multipart.getBoundary(request.headers['content-type']);
 
-    // parse the body
-    var parts = multipart.Parse(bodyBuffer, boundary);
-    context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}}; 
-    context.done();
+            // parse the body
+            var parts = multipart.Parse(bodyBuffer, boundary);
+            context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}}; 
+            context.done();
+    }
+    catch (err)
+    {
+        context.res = { error : err.message }
+    }
 };
