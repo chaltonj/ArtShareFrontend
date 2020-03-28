c130f79d6e107bf140a02abd174cec94fcebb96f
Mitch Webster <mitchell.a.webster@gmail.com>
2020-03-28 16:41:44 -0700

    try to add multipart

--- api/AddPhoto/index.js
+++ api/AddPhoto/index.js
@@ -1,5 +1,12 @@
+var multipart = require("parse-multipart");
+
 module.exports = async function (context, req) {
-    context.res = {
-        body: ["Hey Chris"]
-    }
+    
+    var bodyBuffer = Buffer.from(request.body);
+    var boundary = multipart.getBoundary(request.headers['content-type']);
+
+    // parse the body
+    var parts = multipart.Parse(bodyBuffer, boundary);
+    context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}}; 
+    context.done();
 };
