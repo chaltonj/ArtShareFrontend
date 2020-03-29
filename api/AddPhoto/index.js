6c89f2e291c041b817ddca51a527747e30b87150
Mitch Webster <mitchell.a.webster@gmail.com>
2020-03-28 18:08:16 -0700

    Add .vscode and fix api bug

--- api/AddPhoto/index.js
+++ api/AddPhoto/index.js
@@ -1,18 +1,12 @@
 var multipart = require("parse-multipart");
 
 module.exports = async function (context, req) {
-    
-    try {
-            var bodyBuffer = Buffer.from(request.body);
-            var boundary = multipart.getBoundary(request.headers['content-type']);
+    var bodyBuffer = Buffer.from(req.body);
+    var boundary = multipart.getBoundary(req.headers['content-type']);
 
-            // parse the body
-            var parts = multipart.Parse(bodyBuffer, boundary);
-            context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}}; 
-            context.done();
-    }
-    catch (err)
-    {
-        context.res = { error : "an error has occured :(" }
-    }
+    // parse the body
+    var parts = multipart.Parse(bodyBuffer, boundary);
+    context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}}; 
+    context.done();
+    
 };
