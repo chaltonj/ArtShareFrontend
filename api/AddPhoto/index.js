2526324e573be386199cab9e3317de8de1633934
Mitch Webster <mitchell.a.webster@gmail.com>
2020-03-28 23:50:31 -0700

    Update code back to expected

--- api/AddPhoto/index.js
+++ api/AddPhoto/index.js
@@ -2,21 +2,19 @@ var multipart = require("parse-multipart");
 
 module.exports = async function (context, req) {
     var bodyBuffer = Buffer.from(req.body);
-    context.res = { body: req.headers, status: 500};
+
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+
+    // parse the body
+    var parts = multipart.Parse(bodyBuffer, boundary);
+    if (parts == null || parts === undefined || parts.length <= 0)
+    {
+        console.error(parts);
+        context.res = { status: 500, body: "Form parts were undefined" }
+        context.done();
+        return;
+    }
+
+    context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}};
     context.done();
-    // var boundary = multipart.getBoundary(req.headers['content-type']);
-
-    // // parse the body
-    // var parts = multipart.Parse(bodyBuffer, boundary);
-    // if (parts == null || parts === undefined || parts.length <= 0)
-    // {
-    //     console.error(parts);
-    //     context.res = { status: 500, body: "parts were undefined" }
-    // }
-    // else
-    // {
-    //     context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}};
-    // }
-
-    // context.done();
 };
